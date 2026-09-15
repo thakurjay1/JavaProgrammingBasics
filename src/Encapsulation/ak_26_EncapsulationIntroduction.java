93ffc9828c3709c1ffd7897f92aae60ad91a36bb
Jay Thakur <92983729+thakurjay1@users.noreply.github.com>
2026-09-15 21:40:32 +0530

    Update student object usage in EncapsulationIntroduction

--- src/Encapsulation/ak_26_EncapsulationIntroduction.java
+++ src/Encapsulation/ak_26_EncapsulationIntroduction.java
@@ -2,7 +2,6 @@ package Encapsulation;
 
 public class ak_26_EncapsulationIntroduction {
     public static void main(String[] args) {
-
        ak_26_Student obj = new ak_26_Student();
          obj.setAge(22);
         System.out.println(obj.getAge());
